30c38c1be7b773596181fe1f67263c2f1905a90e
Voronoy Alexandr <servermed@gmail.com>
2015-08-13 12:16:38 +0300

    heroku assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   # See email at http://localhost:1080
-  config.action_mailer.smtp_settings = { :address => "http://octohell.herokuapp.com", :port => ENV['SMTP_PORT'] }
+ # config.action_mailer.smtp_settings = { :address => "http://octohell.herokuapp.com", :port => ENV['SMTP_PORT'] }
 end
