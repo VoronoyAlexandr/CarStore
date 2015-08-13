246b5c1340ecd9b2d47e2f2c3f57081e247f586f
Voronoy Alexandr <servermed@gmail.com>
2015-08-13 11:09:07 +0300

    heroku assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.assets.precompile += ['active_admin.css', 'active_admin.js']
 
   config.action_mailer.default_url_options = { host: 'carsstore.herokuapp.com', port: 3000 }
   config.action_mailer.delivery_method = :smtp
